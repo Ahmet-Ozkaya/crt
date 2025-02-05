8c8de65e685353d2b36d49a9c1641cf13d760d4e
Ahmet_Ozkaya <a.ozkaya.gmail.com>
2025-02-05 23:39:58 +1100

    Listing available CRTs

--- employee_profile.php
+++ employee_profile.php
@@ -53,67 +53,68 @@
                     </label>
                 </div>
             </div>
-            <div class="col-md-6">
-                <div class="form-group">
-                    <label>Level</label>
-                    <p class="form-value"><?= htmlspecialchars($employee['level']) ?></p>
-                </div>
+        </div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+                <label>Level</label>
+                <p class="form-value"><?= htmlspecialchars($employee['level']) ?></p>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="form-group">
-                    <label>Bio</label>
-                    <p class="form-value"><?= htmlspecialchars($employee['bio']) ?></p>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="form-group">
-                    <label>Skills</label>
-                    <ul class="form-value">
-                        <?php
-                        $skills = explode(',', $employee['skills']);
-                        foreach ($skills as $skill) {
-                            echo '<li>' . htmlspecialchars(trim($skill)) . '</li>';
-                        }
-                        ?>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="form-group">
-                    <label>work_history</label>
-                    <ul class="form-value">
-                        <?php
-                        $work_history = explode(',', $employee['work_history']);
-                        foreach ($work_history as $work_history) {
-                            echo '<li>' . htmlspecialchars(trim($work_history)) . '</li>';
-                        }
-                        ?>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="form-group">
-                    <label>education</label>
-                    <ul class="form-value">
-                        <?php
-                        $education = explode(',', $employee['education']);
-                        foreach ($education as $education) {
-                            echo '<li>' . htmlspecialchars(trim($education)) . '</li>';
-                        }
-                        ?>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="job-section-btn-wrap">
-            <a href="edit_employee.php?id=<?= $employee['id'] ?>" class="custom-btn btn ms-2">Edit Employee</a>
         </div>
     </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="form-group">
+                <label>Bio</label>
+                <p class="form-value"><?= htmlspecialchars($employee['bio']) ?></p>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="form-group">
+                <label>Skills</label>
+                <ul class="form-value">
+                    <?php
+                    $skills = explode(',', $employee['skills']);
+                    foreach ($skills as $skill) {
+                        echo '<li>' . htmlspecialchars(trim($skill)) . '</li>';
+                    }
+                    ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="form-group">
+                <label>work_history</label>
+                <ul class="form-value">
+                    <?php
+                    $work_history = explode(',', $employee['work_history']);
+                    foreach ($work_history as $work_history) {
+                        echo '<li>' . htmlspecialchars(trim($work_history)) . '</li>';
+                    }
+                    ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="form-group">
+                <label>education</label>
+                <ul class="form-value">
+                    <?php
+                    $education = explode(',', $employee['education']);
+                    foreach ($education as $education) {
+                        echo '<li>' . htmlspecialchars(trim($education)) . '</li>';
+                    }
+                    ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="job-section-btn-wrap">
+        <a href="edit_employee.php?id=<?= $employee['id'] ?>" class="custom-btn btn ms-2">Edit Employee</a>
+    </div>
+</div>
